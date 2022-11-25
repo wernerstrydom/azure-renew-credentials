d0d97c8bc3f9d269662007c7b470df6332dd0350
Werner Strydom <bloudraak@users.noreply.github.com>
2022-11-25 10:02:46 -0800

    Secret Names
    This change ensures that both the username and password is saved following a specific
    convention, typically the server name converted to lowercase, with the "-administrator-login-password" appended.

--- src/RenewCredentials/RenewSqlServerAdminCredentials.cs
+++ src/RenewCredentials/RenewSqlServerAdminCredentials.cs
@@ -9,6 +9,7 @@ using Azure.ResourceManager;
 using Azure.ResourceManager.Sql;
 using Azure.ResourceManager.Sql.Models;
 using Azure.Security.KeyVault.Secrets;
+using Humanizer;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 
@@ -33,22 +34,11 @@ public class RenewSqlServerAdminCredentials
                     try
                     {
                         var password = Password.GeneratePassword();
-                        var patch = new SqlServerPatch
-                        {
-                            AdministratorLoginPassword = password
-                        };
-                        
-                        log.LogInformation("Updating password for SQL Server `{0}` ({1})", sqlServer.Data?.Name,
-                            sqlServer.Data?.Id);
-                        
-                        sqlServer.Update(WaitUntil.Completed, patch);
-                        log.LogInformation("Updated password for SQL Server `{0}` ({1})", sqlServer.Data?.Name,
-                            sqlServer.Data?.Id);
-                        
-                        var secretName = sqlServer.Data?.Name + "-admin-password";
-                        log.LogInformation("Updating secret `{0}` in Key Vault `{1}`", secretName, secretClient.VaultUri);
-                        secretClient.SetSecret(secretName, password);
-                        log.LogInformation("Updated secret `{0}` in Key Vault `{1}`", secretName, secretClient.VaultUri);
+                        SetAdministratorLoginPassword(sqlServer, password, log);
+                        var secretName = sqlServer.Data?.Name.ToLower() + "-" + nameof(sqlServer.Data.AdministratorLoginPassword).Dasherize().ToLower();
+                        SaveSecret(secretClient, secretName, password, log);
+                        secretName = sqlServer.Data?.Name.ToLower() + "-" + nameof(sqlServer.Data.AdministratorLogin).Dasherize().ToLower();
+                        SaveSecret(secretClient, secretName, sqlServer.Data?.AdministratorLogin, log);
                     }
                     catch (Exception e)
                     {
@@ -65,6 +55,29 @@ public class RenewSqlServerAdminCredentials
             log.LogError(e, "Error renewing SQL Server admin credentials");
             throw;
         }
+    }
+
+    private static void SaveSecret(SecretClient client, string name, string value, ILogger log)
+    {
+        log.LogInformation("Updating secret `{0}` in Key Vault `{1}`", name, client.VaultUri);
+        client.SetSecret(name, value);
+        log.LogInformation("Updated secret `{0}` in Key Vault `{1}`", name, client.VaultUri);
+    }
+
+    private static void SetAdministratorLoginPassword(SqlServerResource sqlServer, string password, ILogger log)
+    {
+        log.LogInformation("Updating password for SQL Server `{0}` ({1})", sqlServer.Data?.Name,
+            sqlServer.Data?.Id);
+
+        var patch = new SqlServerPatch
+        {
+            AdministratorLoginPassword = password
+        };
+
+
+        sqlServer.Update(WaitUntil.Completed, patch);
+        log.LogInformation("Updated password for SQL Server `{0}` ({1})", sqlServer.Data?.Name,
+            sqlServer.Data?.Id);
     }
 
     private static SecretClient CreateSecretClient()
