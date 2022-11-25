57087b9d5748cc4825cd7c6e6d2ef3502386ffc9
Werner Strydom <bloudraak@users.noreply.github.com>
2022-11-25 08:50:28 -0800

    Improved Logging
    Added some logging to better understand failures.

--- src/RenewCredentials/RenewSqlServerAdminCredentials.cs
+++ src/RenewCredentials/RenewSqlServerAdminCredentials.cs
@@ -1,12 +1,13 @@
 using System;
-using System.Collections.Immutable;
 using System.Numerics;
 using System.Security.Cryptography;
 using System.Text;
+using Azure;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
 using Azure.ResourceManager.Sql;
+using Azure.ResourceManager.Sql.Models;
 using Azure.Security.KeyVault.Secrets;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
@@ -18,65 +19,90 @@ public class RenewSqlServerAdminCredentials
     [FunctionName("RenewSqlServerAdminCredentials")]
     public static void Run([TimerTrigger("0 */5 * * * *")] TimerInfo timer, ILogger log)
     {
-        //var secretClient = CreateSecretClient();
-        //KeyVaultSecret secret = secretClient.SetSecret("sqladmin", "P@ssw0rd1234");
-
-        log.LogInformation("Password: " + GeneratePassword());
-        
-        var client = new ArmClient(new DefaultAzureCredential());
-        var subscriptions = client.GetSubscriptions();
-        foreach (var subscription in subscriptions)
+        try
         {
-            log.LogInformation("Scanning subscription `{0}` ({1}) for SQL Servers", subscription.Data?.DisplayName,
-                subscription.Data?.SubscriptionId);
-            var sqlServers = subscription.GetSqlServers();
-            foreach (var sqlServer in sqlServers)
-                log.LogInformation($"Server: {sqlServer.Data.Name} - Admin: {sqlServer.Data.AdministratorLogin}");
-            log.LogInformation("Scanned subscription `{0}` ({1}) for SQL Servers", subscription.Data?.DisplayName,
-                subscription.Data?.SubscriptionId);
+            log.LogInformation("Password: " + GeneratePassword());
+
+            var client = new ArmClient(new DefaultAzureCredential());
+            var subscriptions = client.GetSubscriptions();
+            foreach (var subscription in subscriptions)
+            {
+                log.LogInformation("Scanning subscription `{0}` ({1}) for SQL Servers", subscription.Data?.DisplayName,
+                    subscription.Data?.SubscriptionId);
+                var sqlServers = subscription.GetSqlServers();
+                foreach (var sqlServer in sqlServers)
+                    try
+                    {
+                        // var password = GeneratePassword();
+                        // var patch = new SqlServerPatch
+                        // {
+                        //     AdministratorLoginPassword = password
+                        // };
+                        //
+                        // sqlServer.Update(WaitUntil.Completed, patch);
+                        log.LogInformation("Updated password for SQL Server `{0}` ({1})", sqlServer.Data?.Name,
+                            sqlServer.Data?.Id);
+                    }
+                    catch (Exception e)
+                    {
+                        log.LogError(e, "Error settings password for SQL Server `{0}` ({1})", sqlServer.Data?.Name,
+                            sqlServer.Data?.Id);
+                    }
+
+                log.LogInformation("Scanned subscription `{0}` ({1}) for SQL Servers", subscription.Data?.DisplayName,
+                    subscription.Data?.SubscriptionId);
+            }
+        }
+        catch (Exception e)
+        {
+            log.LogError(e, "Error renewing SQL Server admin credentials");
+            throw;
         }
     }
 
     private static SecretClient CreateSecretClient()
-    {
-        // Get the KeyVault uri where secrets will be stored 
-        var environmentValue = Environment.GetEnvironmentVariable("KEYVAULT_URI", EnvironmentVariableTarget.Process);
-        if (string.IsNullOrWhiteSpace(environmentValue))
-            throw new InvalidOperationException(
-                "The application setting `KEYVAULT_URI` is missing, empty or only consists of spaces");
-
-        var options = new SecretClientOptions
         {
-            Retry =
+            // Get the KeyVault uri where secrets will be stored 
+            var environmentValue =
+                Environment.GetEnvironmentVariable("KEYVAULT_URI", EnvironmentVariableTarget.Process);
+            if (string.IsNullOrWhiteSpace(environmentValue))
+                throw new InvalidOperationException(
+                    "The application setting `KEYVAULT_URI` is missing, empty or only consists of spaces");
+
+            var options = new SecretClientOptions
             {
-                Delay = TimeSpan.FromSeconds(2),
-                MaxDelay = TimeSpan.FromSeconds(16),
-                MaxRetries = 5,
-                Mode = RetryMode.Exponential
-            }
-        };
-        var secretClient = new SecretClient(new Uri(environmentValue), new DefaultAzureCredential(), options);
-        return secretClient;
-    }
-
-    private static string GeneratePassword(string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}|[]\\<>?/.,", int length = 64)
-    {
-        var bytes = new byte[length*2];
-        using var generator = RandomNumberGenerator.Create();
-        generator.GetBytes(bytes);
-
-        var builder = new StringBuilder();
-
-        var l = new BigInteger(alphabet.Length);
-        var zero = new BigInteger(0);
-        var n = new BigInteger(bytes);
-        while (n != zero)
-        {
-            var index = n % l;
-            builder.Append(alphabet[(int) index]);
-            n = n / l;
+                Retry =
+                {
+                    Delay = TimeSpan.FromSeconds(2),
+                    MaxDelay = TimeSpan.FromSeconds(16),
+                    MaxRetries = 5,
+                    Mode = RetryMode.Exponential
+                }
+            };
+            var secretClient = new SecretClient(new Uri(environmentValue), new DefaultAzureCredential(), options);
+            return secretClient;
         }
 
-        return builder.ToString();
+        private static string GeneratePassword(
+            string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}|[]\\<>?/.,",
+            int length = 64)
+        {
+            var bytes = new byte[length * 2];
+            using var generator = RandomNumberGenerator.Create();
+            generator.GetBytes(bytes);
+
+            var builder = new StringBuilder();
+
+            var l = new BigInteger(alphabet.Length);
+            var zero = new BigInteger(0);
+            var n = new BigInteger(bytes);
+            while (n != zero)
+            {
+                var index = n % l;
+                builder.Append(alphabet[(int)index]);
+                n = n / l;
+            }
+
+            return builder.ToString();
+        }
     }
-}
