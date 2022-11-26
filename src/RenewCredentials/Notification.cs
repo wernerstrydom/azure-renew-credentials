4123164726efbdfd5cc2fccb73d7c2fdaaeda116
Werner Strydom <bloudraak@users.noreply.github.com>
2022-11-26 11:41:26 -0800

    Adds Slack Notification Support

--- src/RenewCredentials/Notification.cs
+++ src/RenewCredentials/Notification.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Net.Http;
+using System.Text;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 
@@ -9,6 +12,23 @@ public class Notification
     public void Run([QueueTrigger("notifications", Connection = "AzureWebJobsStorage")] string notification,
         ILogger log)
     {
-        log.LogInformation($"C# Queue trigger function processed: {notification}");
+        var message = $"Notification: {notification}";
+        
+        var endpoint = Environment.GetEnvironmentVariable("SlackEndpoint");
+        if (string.IsNullOrEmpty(endpoint))
+        {
+            // No endpoint configured, so just log the message
+            log.LogWarning("SlackEndpoint is not set");
+            return;
+        }
+        
+        // send a slack message  
+        using var client = HttpClientFactory.Create();
+        using var request = new HttpRequestMessage();
+        request.Method = HttpMethod.Post;
+        request.RequestUri = new Uri(endpoint);
+        request.Content = new StringContent(notification, Encoding.UTF8, "application/json");
+        using var response = client.SendAsync(request).Result;
+        response.EnsureSuccessStatusCode();
     }
 }
