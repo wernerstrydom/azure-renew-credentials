73a54157e7767b3d33a4a65aabad0d5becffe6a5
Werner Strydom <bloudraak@users.noreply.github.com>
2022-11-26 11:56:25 -0800

    Sends Slack Message in Proper Format

--- src/RenewCredentials/Notification.cs
+++ src/RenewCredentials/Notification.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Text;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace RenewCredentials;
 
@@ -22,13 +23,28 @@ public class Notification
             return;
         }
         
+        // Convert the message to JSON
+        var slackMessage = new SlackMessage
+        {
+            Text = message
+        };
+        
+        var json = JsonConvert.SerializeObject(slackMessage);
+        
+        
         // send a slack message  
         using var client = HttpClientFactory.Create();
         using var request = new HttpRequestMessage();
         request.Method = HttpMethod.Post;
         request.RequestUri = new Uri(endpoint);
-        request.Content = new StringContent(notification, Encoding.UTF8, "application/json");
+        request.Content = new StringContent(json, Encoding.UTF8, "application/json");
         using var response = client.SendAsync(request).Result;
         response.EnsureSuccessStatusCode();
     }
+}
+
+public class SlackMessage
+{
+    [JsonProperty("text")]
+    public string Text { get; set; }
 }
